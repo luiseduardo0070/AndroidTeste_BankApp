1d738d9167a57b98b8b294be16af16332fc28e09
jackmec0070 <46676309+jackmec0070@users.noreply.github.com>
2019-01-22 16:27:02 -0200

    Activities and button functions created

--- app/src/main/java/com/everis/androidteste_bankapp/LoginActivity.java
+++ app/src/main/java/com/everis/androidteste_bankapp/LoginActivity.java
@@ -1,13 +1,38 @@
 package com.everis.androidteste_bankapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        iniciarVariaveis();
+        iniciarAcao();
+    }
+
+
+    private void iniciarVariaveis() {
+        btn_login = findViewById(R.id.btn_login);
+    }
+
+    private void iniciarAcao() {
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), DataActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
